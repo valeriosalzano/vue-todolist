d0dc840c6c70e15893d16e27378ef78f13ace954
Valerio Salzano <vlr.slzn@gmail.com>
2023-03-16 16:47:50 +0100

    milestone 2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-// Rifare l’esercizio della to do list.
-// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
-// - text, una stringa che indica il testo del todo
-// - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
-// MILESTONE 1
-// Stampare all’interno di una lista HTML un item per ogni todo.
-// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 // MILESTONE 2
 // Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
 // MILESTONE 3
@@ -31,6 +24,9 @@ const { createApp } = Vue
           list.push(item);
         }
         return list;
+      },
+      deleteItem (index){
+        this.list.splice(index,1);
       }
     },
     mounted(){
